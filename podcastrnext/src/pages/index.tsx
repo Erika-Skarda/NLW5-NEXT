f4bc81c2c182b085f287fce1fdfec16f55d988fa
Erika-Skarda <erikaskarda@yahoo.com.br>
2021-05-24 16:33:10 -0300

    Second commit

--- podcastrnext/src/pages/index.tsx
+++ podcastrnext/src/pages/index.tsx
@@ -1,19 +1,160 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
+import { GetStaticProps } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { api } from '../services/api' ;
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+import styles from './home.module.scss';
 
-export default function Home(props) {
-  console.log(props.episodes)
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  // description: string;
+  members: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
+}
+
+type HomeProps = {
+  latestEpisodes: Array<Episode>;
+  allEpisodes: Episode[];
+}
+
+export default function Home({ latestEpisodes, allEpisodes } : HomeProps) {
   //SPA
   // useEffect(() => {
   //   fetch(' http://localhost:3002/episodes')
   //     .then(response => response.json())
   //     .then(data => console.log(data))
   // },[]);
-
+// console.log("aqui", JSON.stringify(props.episodes))
   return (
-    <div>
-     Index
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos lançamentos</h2>
+
+        <ul>
+          {latestEpisodes.map(episode => {
+            return (
+              <li key={episode.id}>
+                <Image 
+                  width={192}
+                  height={192}
+                  objectFit="cover"
+                  src={episode.thumbnail} 
+                  alt={episode.title}
+                />
+
+                <div className={styles.episodeDetails}>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
+                  <p>{episode.members}</p>
+                  <span>{episode.publishedAt}</span>
+                  <span>{episode.durationAsString}</span>
+                </div>
+
+                <button type="button">
+                  <img src="/play-green.svg" alt=""/>
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+
+      <section className={styles.allEpisodes}>
+          <h2>Todos episódios</h2>
+
+          <table cellSpacing={0}>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Podcast</th>
+                <th>Integrantes</th>
+                <th>Data</th>
+                <th>Duração</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {allEpisodes.map(episode => {
+                return ( 
+                  <tr key={episode.id}>
+                    <td style={{width: 72}}>
+                      <Image
+                        width={192}
+                        height={192}
+                        src={episode.thumbnail} 
+                        alt={episode.title}
+                        objectFit="cover"
+                      />
+                    </td>
+                    <td>
+                      <Link href={`/episodes/${episode.id}`}>
+                        <a>{episode.title}</a>
+                      </Link>
+                    </td>
+                    <td>{episode.members}</td>
+                    <td style={{width: 100}}>{episode.publishedAt}</td>
+                    <td>{episode.durationAsString}</td>
+                    <td>
+                      <button type="button">
+                        <img src="/play-green.svg" alt="Tocar episódio"/>
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+      </section>
     </div>
-  )
+  );
+}
+
+
+//Executa antes de exectuar pg p usuário --SSG -> SÓ FUNCIONA EM PRODUÇÃO(npm build) e rodar  como npm run start no lugar de dev
+//revalidate é a frequencia de requisição.
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      // description: episode.description,
+      url: episode.file.url,
+    }
+  })
+
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length);
+
+  return {
+    props: {
+      latestEpisodes,
+      allEpisodes
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
 
 //Executa antes de exectuar pg p usuário --SSR
@@ -27,17 +168,3 @@ export default function Home(props) {
 //     }
 //   }
 // }
-
-//Executa antes de exectuar pg p usuário --SSG -> SÓ FUNCIONA EM PRODUÇÃO(npm build) e rodar  como npm run start no lugar de dev
-//revalidate é a frequencia de requisição.
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3002/episodes')
-  const data = await response.json()
-  
-  return {
-    props: {
-      episodes: data,
-    },
-    revalidate: 60 * 60 * 8,
-  }
-}
